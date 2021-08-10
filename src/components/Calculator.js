1d2dad32595576d7e608931fd68e3d6dbcc434e1
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-10 12:51:31 -0500

    Add calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return <div>helo</div>;
+  }
+}
