36c82358a3805e4d469a54bf67b86128de3f5373
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-10 16:45:16 -0500

    Add calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,70 @@
 import React from 'react';
+import './calculatorStyle.scss';
+import CalculatorButton from './buttons/CalculatorBtn';
+import Display from './display/Display';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      currentValue: '0',
+      numbers: Array.from(Array(10).keys()),
+      special: ['AC', '+/-', '%'],
+      operators: ['/', '*', '-', '+', '='],
+      decimalDot: '.',
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler = (e) => {
+    this.setState({ currentValue: e.target.value });
   }
 
   render() {
-    return <div>helo</div>;
+    const {
+      numbers, decimalDot, special, operators, currentValue,
+    } = this.state;
+    const numBtns = numbers.reverse().map((num) => (
+      <CalculatorButton
+        key={num}
+        number={num.toString()}
+        clickHandler={this.clickHandler}
+      />
+    ));
+    const specialBtns = special.map((btn) => (
+      <CalculatorButton
+        key={btn}
+        number={btn.toString()}
+        clickHandler={this.clickHandler}
+      />
+    ));
+    const operatorBtns = operators.map((btn) => (
+      <CalculatorButton
+        key={btn}
+        number={btn.toString()}
+        clickHandler={this.clickHandler}
+      />
+    ));
+    return (
+      <div className="main-container">
+        <Display number={currentValue} />
+        <ul className="special-container">
+          {specialBtns}
+        </ul>
+        <ul className="numbers-container">
+          {numBtns}
+          <CalculatorButton
+            className="dot-btn"
+            type="button"
+            clickHandler={this.clickHandler}
+            number={decimalDot}
+          />
+        </ul>
+        <ul className="operator-container">
+          {operatorBtns}
+        </ul>
+      </div>
+    );
   }
 }
