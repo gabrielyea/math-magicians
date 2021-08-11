97b270eef44ec41d505acf3b82ea9358576b8c04
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-11 14:35:58 -0500

    Add functionality to display

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,22 +1,27 @@
 import './displayStyle.scss';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Display extends React.Component {
+  setDisplay = (numbers) => {
+    if (numbers.number.next) {
+      return numbers.number.next;
+    }
+    if (numbers.number.total) {
+      return numbers.number.total;
+    }
+    return 0;
+  }
+
   render() {
-    const { number } = this.props;
+    const result = (this.setDisplay(this.props));
     return (
       <div
         className="display-container"
-        value={number}
+        value={result}
       >
-        {number}
+        {result}
       </div>
     );
   }
 }
-
-Display.propTypes = {
-  number: PropTypes.string.isRequired,
-};
