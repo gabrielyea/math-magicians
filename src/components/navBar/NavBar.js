9893d387ce6cffd1d9b9cdcc16057fbfbfa8f806
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-17 13:43:15 -0500

    Create NavBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -2,16 +2,47 @@
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './navBar.module.scss';
 
 const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Qoute',
+    },
+  ];
+
+  const createLinks = () => {
+    return links.map((link) => (
+      <li key={link.id}>
+        <NavLink
+          to={link.path}
+          activeClassName="active-link"
+          exact
+        >
+          {link.text}
+        </NavLink>
+      </li>
+    ));
+  };
+
   return (
     <section className={styles.navBar}>
       <h1>Math Magicians</h1>
       <ul>
-        <li><a href="#section">Home</a></li>
-        <li><a href="#section">Calculator</a></li>
-        <li><a href="#section">Quote</a></li>
+        {createLinks()}
       </ul>
     </section>
   );
