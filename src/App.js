3bcaec05286809e14d4ca8f2929c7f19c5e7b1d8
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-17 12:41:23 -0500

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Calculator from './components/calculator/Calculator';
+import NavBar from './components/navBar/NavBar';
 
 const App = () => (
   <div className="App">
+    <NavBar />
     <Calculator />
   </div>
 );
